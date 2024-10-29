bebf04cc3982599a198f04160ec64832446f6c8f
lakshman <lakshman.automation@gmail.com>
2024-10-29 11:30:51 +0530

    succesfully extracted form data.

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Popup = () => {
     // const { checkboxes, setCheckbox, dropdown, setDropdown, fromDate, toDate, setFromDate, setToDate, togglePopup, updateCheckbox,
@@ -8,74 +8,68 @@ const Popup = () => {
     //   dropDownMap,
     //   updateToDate, updateDatesForSmallestIndex, smallestIndexObject } = useStore();
     const formRef = useRef(null);
-  
+
     // console.log("section", section)
     // console.log("fromDate", fromDate)
     // console.log("toDate", toDate)
     // console.log("smallestIndexObject", smallestIndexObject)
-  
-  
+    const SECTIONS_COUNT = 5
+    const sections = useState(Array.from({length:SECTIONS_COUNT}, (_,i)=>i+1));
+    console.log(sections);
+
     const handleOk = (e) => {
-      e.preventDefault();
-      const formData = new FormData(e.target);
-      const formObj = {};
-      formData.entries(entry => {
-        console.log(entry);
-      })
-      
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formObj = {};
+        formData.entries().forEach(entry => {
+            formObj[entry[0]] = entry[1];
+            // console.log(entry);
+        })
+        console.log(formObj);
       // togglePopup();
     };
-  
+
     return (
         <div className="popup" style={{ width: '900px' }}>
             <h2>MultipleSelection</h2>
-            <form action="" method="post" ref={formRef}>
+            <form  onSubmit={handleOk} ref={formRef}>
                 <table>
-                    <tr>
-                        <th>Checkbox</th>
-                        <th>Option</th>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>Description</th>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Checkbox</th>
+                            <th>Option</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
                     <tbody>
-                    {
-                        [1,2,3,4,5].map(value => (
-                            <tr>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                        ))
-                            
-                    }
-                        <input type="text" />
-                        <button type="submit" onSubmit={handleOk}>Ok</button>
+                        {
+                            sections.map(value => (
+                                <tr key={value}>
+                                    <td>
+                                        <input type="checkbox" name={`checkbox_${value}`}/>
+                                    </td>
+                                    <td>
+                                        <select name={`dropdown_${value}`}>
+                                            <option value="" defaultValue={""}>Select</option>
+                                            <option value="[]">[]</option>
+                                            <option value="<">{'<'}</option>
+                                            <option value=">">{'>'}</option>
+                                            <option value="<=">{'<='}</option>
+                                            <option value="=">{'='}</option>
+                                            <option value=">=">{'>='}</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            ))      
+                        }
                     </tbody>
-                        
                 </table>
+                <button type="submit">Ok</button>
             </form>
         </div>
     );
 };
 
   export default Popup;
-  {
-    [1,2,3,4,5].map(value => (
-        <tr key={value}>
-            <td>
-                <input type="checkbox" name={`checkbox_${value}`}/>
-            </td>
-            <td>
-                <select name={`dropdown_${value`}} >
-                    <option value="" defaultValue={""}>Select</option>
-                    <option value="[]">[]</option>
-                    <option value=">">{>}</option>
-                    <option value="<">{<}</option>
-                    <option value=">=">{>=}</option>
-                    <option value="<=">{<=}</option>
-                    <option value="!=">{!=}</option>
-                </select>
-            </td>
-        </tr>))
-}
