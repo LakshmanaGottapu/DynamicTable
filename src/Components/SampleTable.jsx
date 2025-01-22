1c0eb224260048f8e6b4fa0545a87da713ae1ef4
lakshman <lakshman.automation@gmail.com>
2025-01-23 00:00:10 +0530

    achieved proper InLineCell Editing.

--- src/Components/SampleTable.jsx
+++ src/Components/SampleTable.jsx
@@ -9,10 +9,10 @@ import {
 } from "@devexpress/dx-react-grid-material-ui";
 import { Select } from 'antd';
 const columns = [
-    { name: "id", title: "ID", isId: true, align: 'center' },
-    { name: 'username', title: "UserName", editingEnabled: false, width: '20rem', align: 'center' },
-    { name: "age", title: "Age", align: 'center' },
-    { name: "occupation", title: "Occupation", align: 'center' }
+    { name: "id", title: "ID", isId: true, align: 'left' },
+    { name: 'username', title: "UserName", editingEnabled: false, width: '20rem', align: 'left' },
+    { name: "age", title: "Age", align: 'left' },
+    { name: "occupation", title: "Occupation", align: 'left' }
 ]
 const getRowId = row => row.id
 function SampleTable() {
@@ -23,6 +23,8 @@ function SampleTable() {
     ])
     const [columnExtensions] = useState(columns.map(column => ({ columnName: column.name, width: column.width, align: column.align, wordWrapEnabled: column.wordWrapEnabled })))
     const [editColumnExtensions] = useState(columns.map(column => ({ columnName: column.name, editingEnabled: column.editingEnabled })));
+    // const [editingRowIds, setEditingRowIds] = useState([1]);
+    const [editingCells, setEditingCells] = useState([])
     function onCommitChanges({ added, changed, deleted }) {
         let changedRows;
         if (added) {
@@ -33,28 +35,18 @@ function SampleTable() {
             changedRows = rows.map(row => changed[row.id] ? { ...row, ...changed[row.id] } : row)
         if (deleted)
             changedRows = rows.filter(row => deleted.filter(deletedId => deletedId == row.id).length == 0)
+        console.log(changedRows);
         setRows(changedRows);
     }
     function rowChange(row, value, columnName) {
+        console.log('row change')
         let newRows;
         newRows = rows.map(r => r.id === row.id ? { ...r, [columnName]: value } : r)
         console.log(newRows);
         setRows(newRows);
     }
     // const editableCell;
-    const renderEditableCell = ({ row, column, value, editingEnabled, onValueChange, autoFocus, onFocus, onBlur, onKeyDown }) => {
-        console.log(onValueChange);
-        return (
-            <input key={row.id}
-                type="text"
-                value={value || ""}
-                onChange={(e) => onValueChange(e.target.value)}
-            // autoFocus={autoFocus}
-            // style={{border:'none'}}
-            // onBlur={(e)=>console.log(e)}
-            />
-        )
-    }
+    
     const OccupationFormatter = (formatObject) => {
         // console.log({ formatObject })
         const { column, row, value } = formatObject
@@ -62,7 +54,7 @@ function SampleTable() {
     }
     const OccupationEditor = (editObject) => {
         const { value, onValueChange, autoFocus, row, column, disabled, onBlur, onFocus,onKeyDown, } = editObject
-        console.log({editObject})
+        // console.log({editObject})
         return (
             <Select defaultValue={value} 
                 onChange={onValueChange}
@@ -79,13 +71,46 @@ function SampleTable() {
             <DataTypeProvider formatterComponent={OccupationFormatter} editorComponent={OccupationEditor} {...props} />
         )
     }
-    function RenderCell({ children, row, value, column }) {
-        console.log({children})
+    function RenderCell(obj) {
+        const { children, row, value, column, onClick } = obj
+        const editable = !column.isId && (column.editingEnabled == undefined ? true : column.editingEnabled)
         return (
-            <Table.Cell style={{ backgroundColor: column.editingEnabled ? 'lightgray' : 'white', textAlign: column.align }} >
+            <Table.Cell style={{ backgroundColor: editable ? '' : 'lightgray', cursor: editable ? 'pointer' : '', textAlign: column.align, border:'1px solid black' }} tabIndex={0} onFocus={onClick}>
                 {children || value}
             </Table.Cell>
         )
+    }
+    const renderEditableCell = ({ row, column, value, editingEnabled, onValueChange, autoFocus, onFocus, onBlur, onKeyDown }) => {
+        console.log(onBlur)
+        return (
+            <Table.Cell style={{ textAlign: column.align}} tabIndex={0}>
+                <input
+                    tabIndex={0}
+                    type="text"
+                    autoFocus
+                    defaultValue={value || ""}
+                    // onChange={(e) => onValueChange(e.target.value)}
+                    style={{border:'none'}}
+                    onBlur={(e) => {
+                        onValueChange(e.target.value); setEditingCells([])
+                    }
+                    }
+                    onKeyDown={(e)=>{
+                        if(e.code=="Enter"){
+                            onValueChange(e.target.value);
+                            setEditingCells([]);
+                        }
+                    }}
+                />
+            </Table.Cell>
+        )
+    }
+    function checkDisabledRows(cells){
+        const newCells = cells.filter( cell => {
+            const col = columns.filter(column => column.name==cell.columnName)[0]
+            return !col.isId && (col.editingEnabled == undefined ? true : col.editingEnabled)
+        })
+        setEditingCells(newCells);
     }
     return (
         <Grid
@@ -102,17 +127,19 @@ function SampleTable() {
             <TableHeaderRow />
             <EditingState
                 onCommitChanges={onCommitChanges}
-                // editingRowIds={[0]} 
-                // createRowChange={rowChange}
+                onEditingCellsChange={checkDisabledRows}
+                editingCells={editingCells} 
+                // onEditingRowIdsChange={setEditingRowIds}
+                createRowChange={rowChange}
                 columnExtensions={editColumnExtensions}
             />
             <TableEditRow />
-            <TableEditColumn showDeleteCommand showEditCommand />
-            {/* <TableInlineCellEditing 
+            <TableEditColumn />
+            <TableInlineCellEditing 
                 startEditAction='click'
                 selectTextOnEditStart={true}
                 cellComponent={renderEditableCell}
-            /> */}
+            />
         </Grid>
     )
 }
